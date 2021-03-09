db3f76e7086a7e7649df1941790d6f86babc3a00
Clement BERARD <contact@clementberard.com>
2021-03-09 06:44:56 +0100

    up

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,79 @@
 <script>
-	export let name;
+    let curlInput;
+    let resultQuery = '';
+    let resultVariables = '';
+    let hasBadFormat = false;
 
-	function handleClick() {
-		// const to = getFileContent('./input.txt');
-		const regex = /(--data-raw)(.)+/gm;
-		const found = to.match(regex)[0];
-		const t1 = found.substring(12)
-		const t2 = t1.slice(0, -3)
-		const {variables, query} = JSON.parse(t2)
-		console.log('variables', variables);
-		console.log(query);
-	}
+    function selectTextOnFocus(node) {
+
+        const handleFocus = event => {
+            node && typeof node.select === 'function' && node.select()
+        }
+
+        node.addEventListener('focus', handleFocus)
+
+        return {
+            destroy() {
+                node.removeEventListener('focus', handleFocus)
+            }
+        }
+    }
+
+    function proceed() {
+        hasBadFormat = false;
+        if (curlInput) {
+            try {
+                const regex = /(--data-raw)(.)+/gm;
+                const found = curlInput.match(regex)[0];
+                const t1 = found.substring(12)
+                const t2 = t1.slice(0, -3)
+                const {variables, query} = JSON.parse(t2)
+                console.log('query', query);
+                console.log('variables', variables);
+                resultQuery = query;
+                resultVariables = JSON.stringify(variables, null, 2);
+            } catch (e) {
+                console.error('bad format');
+                hasBadFormat = true;
+            }
+        }
+    }
 </script>
 
 <main>
-	<h1>Hello</h1>
-	<textarea class="input-code-curl"/>
-	<div class="result">
-		<div class="variables">
-			fdsafd
-		</div>
-	</div>
+
+    <div>
+        <h1 class="title is-1">Hello</h1>
+        <div class="curl-entry">
+            <textarea placeholder="curl" bind:value={curlInput} on:keyup={proceed}></textarea>
+        </div>
+        {#if hasBadFormat}
+            <div class="notification is-danger">
+                Bad format
+            </div>
+        {/if}
+        <div class="results">
+            <h3 class="title is-3">Result</h3>
+            <div class="query">
+                <textarea placeholder="query/mutation" use:selectTextOnFocus>{resultQuery}</textarea>
+            </div>
+            <div class="query">
+                <textarea placeholder="variables" use:selectTextOnFocus>{resultVariables}</textarea>
+            </div>
+        </div>
+    </div>
 </main>
 
 <style type="text/scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: none;
+    margin: 0 auto;
 
-		textarea.input-code-curl {
-			width: 60vw;
-			height: 400px;
-		}
-		.result {
-			.variables {
-				background-color: blue;
-			}
-		}
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    textarea {
+      width: 60vw;
+      height: 400px;
+    }
+  }
 </style>
